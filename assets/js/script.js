b72ad9971b2a10e34c706dfe2b3ef11608144c20
Arka Roy <i.arkaroy@gmail.com>
2015-04-09 21:27:56 +0530

    Added contact form

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,10 +9,53 @@ $(document).ready(function () {
             $(this).addClass("menu-active");
         }
     });
+
+    $(".contact-form").on("submit", function (event) {
+        event.preventDefault();
+        
+        var name_regex = /^([a-zA-Z0-9 ])+$/;
+        var email_regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        
+        var name = $(this).find("#name").val();
+        var email = $(this).find("#email").val();
+        if(!name_regex.test(name)) {
+            $(this).find("#name").addClass("error");
+            return false;
+        } else {
+            $(this).find("#name").removeClass("error");
+        }
+        if(!email_regex.test(email)) {
+            $(this).find("#email").addClass("error");
+            return false;
+        } else {
+            $(this).find("#email").removeClass("error");
+        }
+        
+        var form_data = $(this).serialize();
+        $.ajax({
+            url: "//formspree.io/i.arkaroy@gmail.com",
+            method: "POST",
+            data: form_data,
+            dataType: "json",
+            beforeSend: function(){
+                $(".contact-form").hide();
+                $(".form-sending").show();
+            },
+            success: function(response){
+                if(response.success == 'email sent') {
+                    $(".form-sending").hide();
+                    $(".form-sent").show();
+                }
+            },
+            error: function(){
+                
+            }
+        });
+    });
 });
 
-$(window).resize(function(){
-    if($(window).width() > 768) {
+$(window).resize(function () {
+    if ($(window).width() > 768) {
         $(".main-nav").show();
     } else {
         $(".main-nav").hide();

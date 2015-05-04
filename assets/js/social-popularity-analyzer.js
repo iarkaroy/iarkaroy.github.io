7351560532437bf7940266b5e69644399188a846
Arka Roy <i.arkaroy@gmail.com>
2015-05-04 09:11:00 +0530

    Updated social analyzer

--- assets/js/social-popularity-analyzer.js
+++ assets/js/social-popularity-analyzer.js
@@ -15,22 +15,118 @@ $(document).ready(function(){
 });
 
 function getSocialPopularity(url) {
-    var twitter_count = getTwitterCount(url);
-    alert(twitter_count);
+    getTwitterPopularity(url, function(response) {
+        $("#twitter-share").html(response.count ? response.count : 0);
+        $(".spa-result.twitter").fadeIn();
+    });
+    getFacebookPopularity(url, function(response) {
+        $("#fb-share").html(response[0].share_count ? response[0].share_count : 0);
+        $("#fb-like").html(response[0].share_count ? response[0].like_count : 0);
+        $("#fb-comment").html(response[0].share_count ? response[0].comment_count : 0);
+        $("#fb-total").html(response[0].share_count ? response[0].total_count : 0);
+        $(".spa-result.facebook").fadeIn();
+    });
+    getLinkedInPopularity(url, function(response){
+        $("#linkedin-share").html(response.count ? response.count : 0);
+        $(".spa-result.linkedin").fadeIn();
+    });
+    getPinterestPopularity(url, function(response){
+        $("#pinterest-share").html(response.count ? response.count : 0);
+        $(".spa-result.pinterest").fadeIn();
+    });
+    /*
+    getStumbleUponPopularity(url, function(response){
+        $("#stumbleupon-view").html(response.result.url);
+        $(".spa-result.stumbleupon").fadeIn();
+    });
+    */
+    getBufferPopularity(url, function(response){
+        console.log(response);
+    });
 }
 
-function getTwitterCount(url) {
+function getTwitterPopularity(url, callback) {
     $.ajax({
         url: "https://cdn.api.twitter.com/1/urls/count.json",
         method: "GET",
         data: {"url":url},
+        dataType: "jsonp",
+        success: function(response) {
+            callback(response);
+        }
+    });
+}
+
+function getFacebookPopularity(url, callback) {
+    $.ajax({
+        url: "http://api.facebook.com/restserver.php",
+        method: "GET",
+        data: {"method":"links.getStats", "urls":url, "format":"json"},
         dataType: "json",
         success: function(response) {
-            if(response.count) {
-                return response.count;
-            } else {
-                return 0;
-            }
-        },
+            callback(response);
+        }
+    });
+}
+
+function getLinkedInPopularity(url, callback) {
+    $.ajax({
+        url: "https://www.linkedin.com/countserv/count/share",
+        method: "GET",
+        data: {"url":url, "format":"jsonp"},
+        dataType: "jsonp",
+        success: function(response) {
+            callback(response);
+        }
+    });
+}
+
+function getGooglePlusPopularity(url, callback) {
+    /*
+    $.ajax({
+        url: "https://www.linkedin.com/countserv/count/share",
+        method: "GET",
+        data: {"url":url, "format":"jsonp"},
+        dataType: "jsonp",
+        success: function(response) {
+            callback(response);
+        }
+    });
+    */
+}
+
+function getPinterestPopularity(url, callback) {
+    $.ajax({
+        url: "https://api.pinterest.com/v1/urls/count.json",
+        method: "GET",
+        data: {"callback":"jsonp", "url":url},
+        dataType: "jsonp",
+        success: function(response) {
+            callback(response);
+        }
+    });
+}
+
+function getStumbleUponPopularity(url, callback) {
+    $.ajax({
+        url: "https://www.stumbleupon.com/services/1.01/badge.getinfo",
+        method: "GET",
+        data: {"url":url},
+        dataType: "json",
+        success: function(response) {
+            callback(response);
+        }
+    });
+}
+
+function getBufferPopularity(url, callback) {
+    $.ajax({
+        url: "https://api.bufferapp.com/1/links/shares.json",
+        method: "GET",
+        data: {"url":url},
+        dataType: "json",
+        success: function(response) {
+            callback(response);
+        }
     });
 }

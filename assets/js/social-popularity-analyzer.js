37dd4fda0e78232b8ccb5150e386c6a01a437b91
Arka Roy <i.arkaroy@gmail.com>
2015-05-05 21:09:48 +0530

    Updated social popularity tool

--- assets/js/social-popularity-analyzer.js
+++ assets/js/social-popularity-analyzer.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
-    $(".social-popularity-analyzer-form").on("submit", function(ev){
+$(document).ready(function () {
+
+    if ($(".social-share").length > 0) {
+        self_url = $(".social-share").attr('data-url');
+        getSocialPopularity(self_url);
+    }
+
+    $(".social-popularity-analyzer-form").on("submit", function (ev) {
         ev.preventDefault();
         var url = $("#url").val();
         var urlRegExp = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-        if(!urlRegExp.test(url)) {
+        if (!urlRegExp.test(url)) {
             // Invalid URL
             $("#url").addClass("error");
             $("#url").focus();
@@ -12,121 +18,78 @@ $(document).ready(function(){
             getSocialPopularity(url);
         }
     });
+
 });
 
 function getSocialPopularity(url) {
-    getTwitterPopularity(url, function(response) {
-        $("#twitter-share").html(response.count ? response.count : 0);
-        $(".spa-result.twitter").fadeIn();
-    });
-    getFacebookPopularity(url, function(response) {
-        $("#fb-share").html(response[0].share_count ? response[0].share_count : 0);
-        $("#fb-like").html(response[0].share_count ? response[0].like_count : 0);
-        $("#fb-comment").html(response[0].share_count ? response[0].comment_count : 0);
-        $("#fb-total").html(response[0].share_count ? response[0].total_count : 0);
-        $(".spa-result.facebook").fadeIn();
-    });
-    getLinkedInPopularity(url, function(response){
-        $("#linkedin-share").html(response.count ? response.count : 0);
-        $(".spa-result.linkedin").fadeIn();
-    });
-    getPinterestPopularity(url, function(response){
-        $("#pinterest-share").html(response.count ? response.count : 0);
-        $(".spa-result.pinterest").fadeIn();
-    });
-    /*
-    getStumbleUponPopularity(url, function(response){
-        $("#stumbleupon-view").html(response.result.url);
-        $(".spa-result.stumbleupon").fadeIn();
-    });
-    */
-    getBufferPopularity(url, function(response){
-        console.log(response);
-    });
-}
-
-function getTwitterPopularity(url, callback) {
     $.ajax({
-        url: "https://cdn.api.twitter.com/1/urls/count.json",
+        url: "http://social-popularity.herokuapp.com/",
         method: "GET",
-        data: {"url":url},
+        data: {"url": url},
+        contentType: "application/json",
         dataType: "jsonp",
-        success: function(response) {
-            callback(response);
+        jsonpCallback: 'callback',
+        beforeSend: function () {
+            $(".social-popularity-analyzer-results").fadeOut();
+            $("#social-popularity-analyzer-submit").html('<i class="fa fa-cog fa-spin fa-fw"></i>');
+            $("#social-popularity-analyzer-submit").attr('disabled', 'disabled');
+            $("#social-popularity-analyzer-submit").addClass('busy');
+        },
+        success: function (response) {
+            popularizeSocialInfo(response);
+            $("#url").val('');
+            $(".social-popularity-analyzer-results").fadeIn();
+            $("#social-popularity-analyzer-submit").html('<i class="fa fa-flask fa-fw"></i> <span>Analyze</span>');
+            $("#social-popularity-analyzer-submit").removeAttr('disabled');
+            $("#social-popularity-analyzer-submit").removeClass('busy');
         }
     });
 }
 
-function getFacebookPopularity(url, callback) {
-    $.ajax({
-        url: "http://api.facebook.com/restserver.php",
-        method: "GET",
-        data: {"method":"links.getStats", "urls":url, "format":"json"},
-        dataType: "json",
-        success: function(response) {
-            callback(response);
-        }
-    });
-}
+function popularizeSocialInfo(data) {
+    $("#analysis-report-heading").html("Analysis Report for " + decodeURIComponent(data.url));
 
-function getLinkedInPopularity(url, callback) {
-    $.ajax({
-        url: "https://www.linkedin.com/countserv/count/share",
-        method: "GET",
-        data: {"url":url, "format":"jsonp"},
-        dataType: "jsonp",
-        success: function(response) {
-            callback(response);
-        }
-    });
-}
+    // Facebook
+    $("#facebook-share").html(data.social.facebook.share);
+    $("#facebook-like").html(data.social.facebook.like);
+    $("#facebook-comment").html(data.social.facebook.comment);
+    $("#facebook-total").html(data.social.facebook.total);
+    $("#facebook-click").html(data.social.facebook.click);
 
-function getGooglePlusPopularity(url, callback) {
-    /*
-    $.ajax({
-        url: "https://www.linkedin.com/countserv/count/share",
-        method: "GET",
-        data: {"url":url, "format":"jsonp"},
-        dataType: "jsonp",
-        success: function(response) {
-            callback(response);
-        }
-    });
-    */
-}
+    // Twitter
+    $("#twitter-share").html(data.social.twitter.share);
 
-function getPinterestPopularity(url, callback) {
-    $.ajax({
-        url: "https://api.pinterest.com/v1/urls/count.json",
-        method: "GET",
-        data: {"callback":"jsonp", "url":url},
-        dataType: "jsonp",
-        success: function(response) {
-            callback(response);
-        }
-    });
-}
+    // Google Plus
+    $("#googleplus-one").html(data.social.googleplus.one);
 
-function getStumbleUponPopularity(url, callback) {
-    $.ajax({
-        url: "https://www.stumbleupon.com/services/1.01/badge.getinfo",
-        method: "GET",
-        data: {"url":url},
-        dataType: "json",
-        success: function(response) {
-            callback(response);
-        }
-    });
-}
+    // LinkedIn
+    $("#linkedin-share").html(data.social.linkedin.share);
 
-function getBufferPopularity(url, callback) {
-    $.ajax({
-        url: "https://api.bufferapp.com/1/links/shares.json",
-        method: "GET",
-        data: {"url":url},
-        dataType: "jsonp",
-        success: function(response) {
-            callback(response);
-        }
-    });
+    // Pinterest
+    $("#pinterest-pin").html(data.social.pinterest.pin);
+
+    // StumbleUpon
+    $("#stumbleupon-view").html(data.social.stumbleupon.view)
+
+    // Delicious
+    $("#delicious-bookmark").html(data.social.delicious.bookmark);
+
+    // Reddit
+    //$("#reddit-up").html(data.social.reddit.up);
+    //$("#reddit-down").html(data.social.reddit.down);
+    //$("#reddit-score").html(data.social.reddit.score);
+
+    // Buffer
+    $("#buffer-share").html(data.social.buffer.share);
+
+    // All Shares
+    $("#shares-facebook").html(data.social.shares.facebook);
+    $("#shares-twitter").html(data.social.shares.twitter);
+    $("#shares-googleplus").html(data.social.shares.googleplus);
+    $("#shares-linkedin").html(data.social.shares.linkedin);
+    $("#shares-pinterest").html(data.social.shares.pinterest);
+    $("#shares-stumbleupon").html(data.social.shares.stumbleupon);
+    $("#shares-delicious").html(data.social.shares.delicious);
+    //$("#shares-reddit").html(data.social.shares.reddit);
+    $("#shares-buffer").html(data.social.shares.buffer);
 }
